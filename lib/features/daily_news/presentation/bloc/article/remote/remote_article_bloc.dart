0d9968aa67bc5048c9f4aa23d87dd25d10972724
Novjean J Kannathara <novjeanj@gmail.com>
2024-06-09 21:57:07 +0530

    modifying generated retrofit to pull data

--- lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -24,6 +24,7 @@ class RemoteArticlesBloc extends Bloc<RemoteArticlesEvent,RemoteArticlesState> {
     }
 
     if (dataState is DataFailed) {
+      // print(dataState.error!.message);
       emit(
           RemoteArticlesError(dataState.error!)
       );
---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,58 @@
+import 'package:clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(context),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppbar(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        'Daily News',
+        style: TextStyle(color: Colors.black),
+      ),
+      actions: [
+        // GestureDetector(
+        //   onTap: () => _onShowSavedArticle sViewTapped(context),
+        //   child: const Padding(
+        //     padding: EdgeInsets.symmetric(horizontal: 14),
+        //     child: Icon(Icons.bookmark, color: Colors.black),
+        //   ),
+        // ),
+      ],
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
+      builder: (_, state) {
+        if (state is RemoteArticlesLoading) {
+          return const Center(child: CupertinoActivityIndicator());
+        }
+        if (state is RemoteArticlesError) {
+          return const Center(child: Icon(Icons.refresh));
+        }
+        if (state is RemoteArticlesDone) {
+          return ListView.builder(
+              itemCount: state.articles!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('$index'),
+                );
+              });
+        }
+        return const SizedBox();
+      },
+    );
+  }
+}
