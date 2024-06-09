e3fd0fffb03318eb84a47489a8088b4ae385a2f5
Novjean J Kannathara <novjeanj@gmail.com>
2024-06-09 20:47:47 +0530

    bloc setup

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,25 @@
+import 'package:clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:equatable/equatable.dart';
+import 'package:dio/dio.dart';
+
+abstract class RemoteArticlesState extends Equatable{
+  final List<ArticleEntity> ? articles;
+  final DioError ? error;
+
+  RemoteArticlesState(this.articles, this.error);
+
+  @override
+  List<Object> get props => [articles!, error!];
+}
+
+class RemoteArticlesLoading extends RemoteArticlesState {
+  const RemoteArticlesLoading();
+}
+
+class RemoteArticlesDone extends RemoteArticlesState {
+  const RemoteArticlesDone(List<ArticleEntity> article) : super(articles: article);
+}
+
+class RemoteArticlesError extends RemoteArticlesState {
+  const RemoteArticlesError(DioError error) : super(error: error);
+}
